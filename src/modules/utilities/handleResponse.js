428127a2a58f01423d75dbc794f240ea6144b7bb
Dur4nt3 <189136258+Dur4nt3@users.noreply.github.com>
2025-09-21 22:52:57 +0300

    Fix unexpected image source assignment
    Previously when an error was received a source was still assigned to the img tag.
    This problem is resolved with this commit, properly hiding the image when receiving an error.

--- src/modules/utilities/handleResponse.js
+++ src/modules/utilities/handleResponse.js
@@ -1,6 +1,7 @@
 export default function handleResponse(value, imgRef, ignore) {
     if (value === 'error') {
         console.error('An error occurred while fetching your gif!');
+        return;
     }
 
     if (!ignore) {
